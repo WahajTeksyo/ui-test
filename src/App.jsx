1e369423017386e2cb7911bb17895778dd1dee4b
WahajTeksyo <wahaj.ahmed@teksyo.com>
2024-11-11 16:44:31 +0500

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// sidebar 
+import SideBar from './components/Sidebar.jsx'
+
+function App() {
+
+  return (
+    <>
+      <SideBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,8 @@
+
+const Sidebar = () => {
+  return (
+    <div>Sidebaradsfasdfasd</div>
+  )
+}
+
+export default Sidebar
